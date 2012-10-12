c20e65d1ceebee561c69e5f99b45ff4643b87dd9
Stuart Sierra <mail@stuartsierra.com>
2012-10-12 10:46:10 -0400

    Add release metadata to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject com.stuartsierra/flow "0.0.1-SNAPSHOT"
+  :description "Function definitions derived from graph declarations"
+  :url "https://github.com/stuartsierra/flow"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.namespace "0.2.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
                                   [org.clojure/java.classpath "0.2.0"]]}})
