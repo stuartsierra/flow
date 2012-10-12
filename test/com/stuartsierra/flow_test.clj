0128ec03bcc8db27903e34530e1d9cba04552a07
Stuart Sierra <mail@stuartsierra.com>
2012-10-12 10:38:36 -0400

    Add license notice to source files

--- test/com/stuartsierra/flow_test.clj
+++ test/com/stuartsierra/flow_test.clj
@@ -1,3 +1,9 @@
+;; Copyright (c) 2012 Stuart Sierra. All rights reserved. This program
+;; and the accompanying materials are made available under the terms
+;; of the Eclipse Public License v1.0 which accompanies this
+;; distribution, and is available at
+;; http://www.eclipse.org/legal/epl-v10.html
+
 (ns com.stuartsierra.flow-test
   (:use com.stuartsierra.flow
         clojure.test))
