90737295d2c1aced1d9f070d3f7b60708848d05a
Stuart Sierra <mail@stuartsierra.com>
2012-12-13 18:29:09 -0500

    Update tools.namespace version to 0.2.2

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/stuartsierra/flow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/tools.namespace "0.2.1"]]
+  :dependencies [[org.clojure/tools.namespace "0.2.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
                                   [org.clojure/java.classpath "0.2.0"]]
                    :source-paths ["dev"]}})
