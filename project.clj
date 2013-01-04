6f10669d3570455a07b7064dcc1add713ed09559
Donald Clark Jackson <dcj@clark-communications.com>
2013-01-04 00:07:51 -0800

    Add support for lazy-maps

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject com.stuartsierra/flow "0.1.1-SNAPSHOT"
+(defproject com.dcj/flow "0.1.1-SNAPSHOT"  ;; renamed for personal development only!
   :description "Function definitions derived from graph declarations"
   :url "https://github.com/stuartsierra/flow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.namespace "0.2.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
-                                  [org.clojure/java.classpath "0.2.0"]]
+                                  [org.clojure/java.classpath "0.2.0"]
+                                  [de.kotka/lazymap "3.0.0"]]
                    :source-paths ["dev"]}})
 
